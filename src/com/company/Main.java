ef66b9c31db1d771778aff671b63d410a4a0013c
Diansuci6706164048 <37732513+Diansuci6706164048@users.noreply.github.com>
2018-03-29 09:00:10 +0700

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,13 +12,17 @@ public class Main {
 
         fib[0] = 1;
         fib[1] = 1;
+        
+        long sum = 0;
 
         for(int i = 2; i < n; i++) {
             fib[i] = fib[i-1] + fib[i-2];
         }
 
         for (int i = 0; i < n; i++) {
-            System.out.print(fib[i] +  " ");
+            sum = fib[i] + sum;
         }
+        
+        System.out.println(sum)
     }
 }
